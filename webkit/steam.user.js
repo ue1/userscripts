f1bb6ca04c043c1fd9ad8bca85068a5c8b54540a
shangjianzhao <shangjianzhao@limikeji.com>
2019-03-23 11:24:47 +0800

    update steam.js

--- webkit/steam.user.js
+++ webkit/steam.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name Steam评分百分比
-// @description 显示Steam评分(好评如潮位置)百分比
+// @name Steam好评百分比
+// @description 显示Steam好评百分比(好评位置)
 // @namespace https://github.com/ue1/userscripts
-// @version 1.0.1
+// @version 1.0.2
 // @run-at document-end
 // @include http*://store.steampowered.com*
 // @include http*://steamcommunity.com*
@@ -13,17 +13,17 @@
 // ==/UserScript==
 (function () {
     'use strict';
-    var rate = "game_review_summary";
-    var tips = "data-store-tooltip";
-    var span = document.getElementsByTagName("span");
-    for(var i = 0; i < span.length; i++) {
+    var rate = 'game_review_summary';
+    var tips = 'data-tooltip-html';
+    var span = document.getElementsByTagName('span');
+    for (var i = 0; i < span.length; i++) {
         var item = span[i];
-        if(item.className.indexOf(rate) != -1) {
+        if (item.className.indexOf(rate) !== -1) {
             var attr = item.getAttribute(tips);
-            if(!attr) {
+            if (!attr) {
                 attr = item.parentNode.parentNode.getAttribute(tips);
             }
-            if(attr) {
+            if (attr) {
                 var pos = attr.match(/(\d+)%/)[1];
                 item.innerHTML += ' <span class="' + item.className + '">' + pos + '%</span><span class="' + rate + ' no_reviews">/</span><span class="' + rate + ' negative">' + (100 - pos) + '%</span>';
             }
